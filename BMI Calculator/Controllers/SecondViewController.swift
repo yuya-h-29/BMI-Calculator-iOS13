9d40bd2e7087f08026ded7d1412415f23d3b4716
yuya-h-29 <tzou11291996@gmail.com>
2020-07-08 22:28:49 +0900

    working on the second page

--- BMI Calculator/Controllers/SecondViewController.swift
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+        
+        let label = UILabel()
+        label.text = "Hello"
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
+    }
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -35,7 +35,11 @@ class ViewController: UIViewController {
         let height = heightSlider.value
         let weight = weightSlider.value
         let bmi = weight / pow(height, 2)
-        print(bmi)
+        
+        let secondVC = SecondViewController()
+        
+//        self.present(secondVC, animated: )
+        
     }
 }
 
